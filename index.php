1c910f5b541448c51fa3608e355aed9b9f1147b4
yusuf kaygin <yusuf.two000@gmail.com>
2021-12-10 02:08:05 +0300

    oop-3

--- index.php
+++ index.php
@@ -169,3 +169,319 @@ $data = $cheesePizza->constantsInfo();
 
 #class Parent { }
 #class Child extends Parent { }
+
+
+class Ship
+{
+    public $model;
+
+    public function setModel($modelName)
+    {
+        $this->model = $modelName;
+    }
+}
+
+class FastShip extends Ship
+{
+}
+
+$FastShip1 = new FastShip();
+$FastShip1->setModel('Gundam Kyrios');  // gemi'nin model ayarini yaptik
+$FastShip1->model;  // Gundam Kyrios
+
+
+class Parent1
+{
+    public $name = 'parent';
+}
+
+class Child1 extends Parent1
+{
+    public $name = 'child';
+}
+
+$child1 = new Child1();
+$child1->name;  // child
+// eger parent class ve child classta ayni degiskenler veya fonksiyon adlari var ise oncelik olarak turetildigi yere bakar.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+// Protected
+// private'e benzer ama farkli olarak miras alindigi yerde kullanilabilir. Yani yine disaridan cagirmayla kullanilamaz ama cesitli mirasvari iliskilerde kullanilabilir.
+
+class Parent2
+{
+    protected $name = 'yusuf';
+    protected $surname = 'kaygin';
+}
+
+class Child2 extends Parent2
+{
+    public function fullName()
+    {
+        return $this->name . ' ' . $this->surname;
+    }
+}
+
+$child2 = new Child2();
+$child2->fullName();  // yusuf kaygin
+
+// bir degiskenimiz varsa ve bu degiskeni heryerden ulasilabilir yapmak istemiyorsak ama miras yoluylada kullanmak istiyorsak bu noktada protected yardimimiza kosuyor.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+// Final 
+// final sadece classlara ve claslarin methodlarina uygulanabilir. Final kullanildigi yerin tekrar kullanilmasini engeller.Yani kisaca classlar uzerinde miras alinmayi engeller methodlar uzerinde ise miras alindigi class icinde tekrar tanimlanmamayi saglar.
+
+/*final*/
+class Parent3
+{
+    public function sayHello()
+    {
+        return 'halo';
+    }
+}
+
+class Child3 extends Parent3
+{
+    public function sayHello()
+    {
+        return 'hello';
+    }
+}
+
+// parent'a final yazildigi zaman mirasi engeller ve hata verir.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// abstract 
+// classlarda kullanildigi zaman artik o class uzerinden nesne turetemez hale geliriz. Yani kisaca o class'i soyutlamis oluruz peki bu bizim nasil isimize yarar diye soracak olursak soyutladigimiz class'in icinde yazdiklarimizi baska bir class'a miras verdirterek kullanim saglayabiliriz.
+
+abstract class Parent4
+{
+    protected $name = 'yusuf';
+    protected $surname = 'kaygin';
+}
+
+class Child4 extends Parent4
+{
+    public function fullName()
+    {
+        return $this->name . ' ' . $this->surname;
+    }
+}
+
+$child4 = new Child4();
+$child4->fullName();  // yusuf kaygin 
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// interface implement
+// yazdigimiz kodlarin daha anlasilabilir olmasi icin interfaceler hazirlamalari ve bu interfaceleri ilgili classlara implement etmeliyiz.
+// interface icindeki butun fonksiyonlarin implement edildigi classlarca saglanmasi gerekir aksi taktirde o implement hata verir.
+// Acikcasi bunu ufak orneklerle anlatmak biraz zor meslek hayatinda tecrube kazandikca bu konuyu daha iyi kavranacagindan suphem yok.
+
+interface CarInterface
+{
+    public function engine();
+    public function fuel();
+}
+
+class Child5 implements CarInterface
+{
+    public function engine()
+    {
+        return 'engine test';
+    }
+
+    public function fuel()
+    {
+        return 'fuel test';
+    }
+}
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// polymorphism
+// karisik isleri olabildiginde basite indirgeyen yapisi ile gelistirme yapmamizi kolaylik saglar assagidaki ornekteki gibi
+
+interface Shape
+{
+    public function area();
+}
+// sekil adinda interface'i yaziyoruz
+
+class Rectangle implements Shape
+{
+    private $width;
+    private $height;
+
+    public function __construct($width, $height)
+    {
+        $this->height = $height;
+        $this->width  = $width;
+    }
+
+    public function area()
+    {
+        return $this->height * $this->width;
+    }
+}
+
+$rect = new Rectangle(4, 5);
+$rect->area(); // 20
+// constructor'a parametre yollayip ardindan onun uzerinden daha okunabilir bir sekilde hesaplama yaptik.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Type hinting
+// parametreler icinde tipini belirtme olayi. 
+// function my_function(type $param1, type param2, ...) 
+
+class Valid
+{
+    public function __construct($x1, $x2)
+    {
+        $this->number1 = is_numeric($x1) ? $x1 : 'error';
+        $this->number2 = is_numeric($x2) ? $x2 : 'error';
+    }
+}
+
+class CalculatorMachine
+{
+    public function __construct(Valid $valid)
+    {
+        /*
+        print_r($valid);
+
+        output
+        phpOOP\Valid Object
+        (
+            [number1] => 16
+            [number2] => error
+        )
+        */
+    }
+}
+
+$valid = new Valid(16, 'text');
+$calculator = new CalculatorMachine($valid);
+// yukaridaki ornekte bir 2 class kullanarak laraveldeki validatore benzer bir sey yaptik.
+// Class disinda Array, string, int gibi basinda gelen parametrenin tipini degistirebilcegimiz turler vardir.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// instanceof 
+
+class Fruit
+{
+}
+
+$apple = new Fruit();
+$apple instanceof Fruit; // true
+// instanceof bir birleri ile alakali olan nesnelerde true doner.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Destructor
+// ilgli class her calistiginda en son cikarken destructor'da calisir.
+
+
+class Pc
+{
+    private $model;
+    function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    function __destruct()
+    {
+        //echo 'her turlu calisirim';
+    }
+}
+
+$dell = new Pc('dell');
+// yukarida destructor fonksiyonu ne olursa olsun yinede calisir en son.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Const & static
+// sabit degisken anlamina gelir ayrica class'ta nesne turetmeden cekebilme ozelligimiz var asssagidaki ornekte oldugu gibi
+// degiskenin basina dolar isareti koymayiz.
+// class icinde tanimlanmis olan bir const degiskenine class icinde self ile ulasiriz.
+
+class Humans2
+{
+    const name = 'yusuf';
+    static $surname = 'kaygin';
+
+    public function output()
+    {
+        return self::name . ' ' . self::$surname;;
+        // normal degisken olsaydi eger this uzerinden giderdik.
+    }
+}
+
+Humans2::name; // yusuf
+Humans2::$surname; // yusuf  not : evet basinda dolar var
+
+$human2 = new Humans2();
+$human2->output(); // yusuf kaygin
+
+// yukaridaki orneklerde farkli olarak static var statik ile const arasindaki gozle gorulur fark basinda dolar kullanimi. ikiside self ile cagrilir.
+// Static NOT : static ile saklanan bir degisken ram'de saklanir. Boylece bu bize iyi bir performans kazandirir. Ama bunu cokta kullanmamak gerek. Genelde cok sik cagrilan fonksiyonlarda kullanmak mantikli olur.
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Traits
+// her classs icin yanlizca bir extends yapabiliyoruz. Bu siniri traits ile asabiliyoruz.
+
+
+trait Human3
+{
+    public $leg = 2;
+
+    // const name = 'yusuf';   error
+    // static name = 'yusuf';  error
+
+    public function sayHello($name)
+    {
+        return 'hello ' . $name;
+    }
+}
+
+class Student
+{
+    use Human3;
+}
+// new anahtar kelimesiyle turetilemez 
+// const kullanilamaz
+// ilgili class'a dahil etmek icin use anahtar kelimesini kullanmaliyiz.
+
+$joseph = new Student();
+$joseph->leg; // 2
+$joseph->sayHello('yusuf'); // hello yusuf
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Namespace & use
