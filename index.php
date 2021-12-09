9bf1700571fe1a7be272950af9013f0f5b340abf
yusuf kaygin <yusuf.two000@gmail.com>
2021-12-09 16:36:46 +0300

    oop-2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace phpOOP;
 /*
 | 
 |  oop nedir ?
@@ -8,10 +9,14 @@
 | 
 */
 
-#   class Human {}
+class Animals
+{
+}
 // class yapilari bize genel tanimlamalar yapmamiza olanak saglar.
 
+
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
 
 class Human
 {
@@ -33,12 +38,134 @@ $yusuf = new Human();
 // Human class'i uzerinden yusuf degiskenine nesne turetme islemi yaptik.
 // Artik yusuf degiskeni uzerinden human class'inin icindeki ozelliklere ulasabiliriz.
 
-echo $yusuf->leg;  // 2
+$yusuf->leg;  // 2
 # echo $yusuf->idea;  // error
 // -> isareti ile ilgili class'in icindeki verilere ulasabiliriz.
 // public olan degiskeni cekebiliyorken private olani cekemedik.
 
-echo $yusuf->bodyIndex(80, 1.80);  // 20.0405
+$yusuf->bodyIndex(80, 1.80);  // 20.0405
 // class icinde fonksiyonlarda kullanabiliriz.
 
+
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// this anahtar sozcugu
+// this bir class icinde kullanildigi zaman o class'in kendisini kasteder.
+
+class Fish
+{
+    private $name = 'nemo';
+    private $alias = 'kayip balik';
+
+    public function fullName()
+    {
+        return $this->alias . ' ' . $this->name;
+    }
+}
+
+$nemo = new Fish();
+$nemo->fullName();  // kayip balik nemo
+// class icinde kendi ozelliklerinden birini kullanacagimiz zaman this anahtar kelimesini kullaniyoruz.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Zincileme kullanim
+
+class Calculator
+{
+    public $result;
+
+    public function addition($value)
+    {
+        $this->result += $value;
+        return $this;
+    }
+
+    public function ejection($value)
+    {
+        $this->result -= $value;
+        return $this;
+        // this class'i temsil ettigi icin zincirleme yapacaksak class'in son durumunu belirtmeliyiz bunun icin this'i return ediyoruz.
+    }
+}
+
+$process = new Calculator();
+$process->addition(10)->ejection(12)->addition(2)->result; // 0
+// class icinde zincileme kullanma durumlari olabilir. Bunlarda temel gaye class'in son durumunu bildirmemiz.
+// this class'i temsil ettigi icin this'i ilgili fonksiyonlarda return ederek boylece zincirleme kullanima olanak saglayabiliriz.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// __contructor 
+// her class'in yapici fonksiyonu olabilir. Bizim classlardaki yapici fonksiyonumuz constructor'dur.
+// contructor methodu iceren bir classta, o class calistigi zaman ilk olarak constructor method calisir.
+
+class Car
+{
+    private $model = 'Model girilmedi';
+
+    public function __construct($modelName = null)
+    {
+        $this->model = $modelName == null ? $this->model : $modelName;
+        // parametre gonderilmeme durumunu dahil ettik.
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+}
+
+$volvo = new Car('S90');
+$volvo->getModel();  // S90
+// Yukaridaki ornekte goruldugu uzere class'a verdigimiz parametre constructor methodda yakalanabilir.
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// Magic Constants
+// php'de bazi ozel sabitler vardir.
+// ornegin __CLASS__ sabiti icinde bulundugu class'in adini almak icin kullanilir.
+// __LINE__ kullanildigi satiri almak icin
+// __FILE__ kullanildigi yerin tam yolunu almak icin
+// __METHOD__ kullanildigi method adini almak icin
+
+class Pizza
+{
+
+    public function constantsInfo()
+    {
+        $info = [
+            'className'  => __CLASS__,
+            'line'       => __LINE__,
+            'file'       => __FILE__,
+            'method'     => __METHOD__,
+            'short path' => __DIR__,
+            'function'   => __FUNCTION__,
+            'namespace'  => __namespace__,
+            'trait'      => __TRAIT__    // birden fazla extends olayinda traitler kullanilir buda icinde bulundugu trait adini verir.
+        ];
+
+        return $info;
+    }
+}
+
+$cheesePizza = new Pizza();
+$data = $cheesePizza->constantsInfo();
+//print_r($data);
+
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+
+// inherit
+// oop guzellikleri burda basliyor. Dusununki elinizde insanlar her insanin ortak ozelliklei vardir. Her yeni insanda ortak olan ozellikleri yeniden olusturmak yerine bu ortak ozellikleri bir class altinda toplayip insan olusturacagimiz zaman ortak ozellirinden miras alarak olusturmamiz hem daha hizli hemde daha okunabilir yazilim gelistirmemiz acisindan cok onemlidir.
+
+#class Parent { }
+#class Child extends Parent { }
